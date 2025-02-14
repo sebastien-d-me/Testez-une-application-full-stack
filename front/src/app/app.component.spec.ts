ad5601c5de5cfa38b1424e51a4ba32b12ff11477
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-02-14 16:37:36 +0100

    ✅ Test App
    - Ajout de test d'intégration pour vérifier les bonnes requêtes (reprise d'une partie de session)

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -4,9 +4,13 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterTestingModule } from "@angular/router/testing";
 import { expect } from "@jest/globals";
 import { AppComponent } from "./app.component";
+import { SessionService } from "./services/session.service";
 
 
 describe("AppComponent", () => {
+    let service: SessionService;
+
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
@@ -16,8 +20,10 @@ describe("AppComponent", () => {
             ],
             declarations: [
                 AppComponent
-            ]
+            ],
+            providers: [SessionService]
         }).compileComponents();
+        service = TestBed.inject(SessionService);
     });
 
 
@@ -29,5 +35,30 @@ describe("AppComponent", () => {
         const app = fixture.componentInstance;
         
         expect(app).toBeTruthy();
+    });
+
+
+    /// Tests d'intégrations
+    // L'obversable doit bien nous donner la valeur du booléen isLogged false au début
+    it("should return isLogged value to the app", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+
+        app.$isLogged().subscribe(sessionIsLogged => {
+            expect(sessionIsLogged).toBeFalsy();
+        });
+    });
+
+
+    /// La déconnexion doit passer la variable isLogged à false
+    it("should change the value of the isLogged boolean (to false) after logout", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+
+        const checkGetHome = jest.spyOn(service, "logOut");
+
+        app.logout();
+
+        expect(checkGetHome).toHaveBeenCalled();
     });
 });
