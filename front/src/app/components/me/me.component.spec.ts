79e920dc1bca3f6df2f8ffc5f73a1de68e3cc400
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-02-20 19:11:47 +0100

    ✅ Test Me
    - Vérifie que ça appelle bien la fonction

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -7,11 +7,14 @@ import { MatInputModule } from "@angular/material/input";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { SessionService } from "src/app/services/session.service";
 import { MeComponent } from "./me.component";
+import { of } from "rxjs";
+import { UserService } from "src/app/services/user.service";
 
 
 describe("MeComponent", () => {
     let component: MeComponent;
     let fixture: ComponentFixture<MeComponent>;
+    let userService: UserService;
 
     const mockSessionService = {
         sessionInformation: {
@@ -33,9 +36,12 @@ describe("MeComponent", () => {
             providers: [{ 
                 provide: SessionService, 
                 useValue: mockSessionService 
-            }],
+            },
+            UserService
+        ],
         }).compileComponents();
 
+        userService = TestBed.inject(UserService);
         fixture = TestBed.createComponent(MeComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -56,5 +62,18 @@ describe("MeComponent", () => {
         component.back();
 
         expect(checkGetBack).toHaveBeenCalled();
+    });
+
+
+
+
+    // Tests Intégrations
+    /// L'user doit être supprimé
+    it("should call the delete user function", () => {
+        const checkDelete = jest.spyOn(userService, "delete").mockReturnValue(of({}));
+    
+        component.delete();
+    
+        expect(checkDelete).toHaveBeenCalledWith(1);
     });
 });
