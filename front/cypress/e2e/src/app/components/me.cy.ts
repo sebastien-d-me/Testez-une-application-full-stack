ce784542d2ce0983865325ac54be25b515b191be
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-02-28 02:08:04 +0100

    ♻️ Nettoyage Tests E2E
    - Début du nettoyage du code

--- front/cypress/e2e/src/app/components/me.cy.ts
+++ front/cypress/e2e/src/app/components/me.cy.ts
@@ -1,39 +1,47 @@
-describe("Me spec", () => {
-    it("Get back to the previous page", () => {
-        cy.visit("/login");
-
+// SRC/APP/COMPONENTS - ME
+describe("Me component", () => {
+    // Test du retour vers la page précédente
+    it("Should get back to the previous page", () => {
         cy.intercept("POST", "/api/auth/login", {
             body: {
                 id: 2,
                 email: "john.doe@test.com",
-                firstName: "John",
                 lastName: "DOE",
-                admin: false
+                firstName: "John",
+                admin: false,
+                createdAt: new Date("2025-02-27 12:00:00"),
             }
         });
 
+        cy.visit("/login");
+        
         cy.get("input[formControlName=email]").type("john.doe@test.com");
         cy.get("input[formControlName=password]").type(`${"test123!"}{enter}{enter}`);
 
-        cy.get("span").contains("Account").click();   
-        cy.wait(100); 
+        cy.get("span").contains("Account").click();    
         cy.get("button.mat-focus-indicator.mat-icon-button.mat-button-base").click();
     
         cy.url().should("include", "/sessions"); 
     });
 
+
+    
+    // Test de récupération des infos d'un utilisateur spécfiique
     it("Show the user details", () => {
         cy.intercept("GET", "/api/user/2", {
             body: {
                 id: 2,
                 email: "john.doe@test.com",
-                firstName: "John",
                 lastName: "DOE",
-                admin: false
+                firstName: "John",
+                admin: false,
+                createdAt: new Date("2025-02-27 12:00:00"),
+                updatedAt: new Date("2025-02-28 13:00:00")
             }
         }).as("meDetails");
 
         cy.get("span").contains("Account").click();   
+        
         cy.wait("@meDetails");
 
         cy.get("p").contains("Name:").should("be.visible").and("contain", "John DOE");
