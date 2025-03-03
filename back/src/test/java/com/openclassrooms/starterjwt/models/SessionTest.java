e818cabcf147be3e6939545dba767ef293d6dc04
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-03 23:48:34 +0100

    ✅ Test Back
    - Ajout du test findUserById sur le UserService
    - Ajout du test delete sur le UserService

---
+++ back/src/test/java/com/openclassrooms/starterjwt/models/SessionTest.java
@@ -0,0 +1,5 @@
+package com.openclassrooms.starterjwt.models;
+
+public class SessionTest {
+    
+}
--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -1,5 +1,55 @@
 package com.openclassrooms.starterjwt.services;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+
 public class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     
+    @Test
+    /// Test - Find user by ID
+    public void testFindById() {
+        // Arrange
+        User user = new User("john.doe@test.com", "DOE", "John", "password", false); 
+
+        // Act
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        User userFound = userService.findById(1L);
+ 
+        // Assert
+        assertEquals("john.doe@test.com", userFound.getEmail());
+    }
+
+    @Test
+    /// Test - Delete user by ID
+    public void testDelete() {
+        // Arrange
+        User user = new User("john.doe@test.com", "DOE", "John", "password", false); 
+
+        // Act
+        doNothing().when(userRepository).deleteById(user.getId());
+        userService.delete(user.getId());
+
+        // Assert
+        verify(userRepository).deleteById(user.getId());
+    }
 }
