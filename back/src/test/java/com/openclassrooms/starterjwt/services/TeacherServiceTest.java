3c5b825c4bb73669bfc404a6a80547c49a7ed9c2
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-04 22:58:57 +0100

    ✅ Test Back
    - Ajout du test findAllsur le TeacherService

--- back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
@@ -7,6 +7,8 @@ import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.repository.TeacherRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.*;
 import org.mockito.*;
@@ -30,7 +32,24 @@ public class TeacherServiceTest {
     @Test
     /// Test - Find all teachers
     public void testFindAll() {
+        // Arrange
+        LocalDateTime createdAt = LocalDateTime.parse("2025-03-03T23:50:00");
+        LocalDateTime updatedAt = LocalDateTime.parse("2025-03-04T00:04:30");
+        LocalDateTime createdAtSecondary = LocalDateTime.parse("2025-04-03T22:30:00");
+        LocalDateTime updatedAtSecondary = LocalDateTime.parse("2025-04-04T22:35:30");
+
+        List<Teacher> teachers = new ArrayList<>();
+        teachers.add(new Teacher(1L, "DOE", "John", createdAt, updatedAt));
+        teachers.add(new Teacher(2L, "DUPONT", "Jean", createdAtSecondary, updatedAtSecondary));
+
+        // Act
+        when(teachRepository.findAll()).thenReturn(teachers);
+        List<Teacher> teachersFound = teacherService.findAll();
+
+        // Assert
+        assertEquals(teachers, teachersFound);
     }
+
 
     @Test
     /// Test - Find teacher by ID
