80a371e2d0d62ed1c2d6713f795f94db04a18199
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-06 00:48:41 +0100

    ✅ Test Back
    - Ajout du test create sur le SessionService
    - Ajout du test delete sur le SessionService

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -29,6 +29,31 @@ public class SessionServiceTest {
 
     
     @Test
+    /// Test - Create a session
+    public void testCreate() {
+        // Arrange
+        Date sessionDate = new Date();
+        LocalDateTime createdAt = LocalDateTime.parse("2025-03-03T23:50:00");
+        LocalDateTime updatedAt = LocalDateTime.parse("2025-03-04T00:04:30");
+
+        Teacher teacher = new Teacher(1L, "DOE", "John", createdAt, updatedAt);
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("martin.petit@test.com", "PETIT", "Martin", "password123", false)); 
+        users.add(new User("leon.bernard@test.com", "BERNARD", "Léon", "password123", false)); 
+
+        Session session = new Session(1L, "Lorem ipsum", sessionDate, "Suspendisse potenti. Praesent orci ligula, rhoncus ut semper ut, ullamcorper eget neque.", teacher, users, createdAt, updatedAt);
+
+        // Act
+        when(sessionRepository.save(session)).thenReturn(session);
+        Session sessionsCreated = sessionService.create(session);
+
+        // Assert
+        assertEquals("Lorem ipsum", sessionsCreated.getName());
+    }
+
+
+    @Test
     /// Test - Find all sessions
     public void testFindAll() {
         // Arrange
@@ -80,5 +105,30 @@ public class SessionServiceTest {
 
         // Assert
         assertEquals("Lorem ipsum", sessionFound.getName());
+    }
+
+
+    @Test
+    /// Test - Delete
+    public void testDelete() {
+        // Arrange
+        Date sessionDate = new Date();
+        LocalDateTime createdAt = LocalDateTime.parse("2025-03-03T23:50:00");
+        LocalDateTime updatedAt = LocalDateTime.parse("2025-03-04T00:04:30");
+
+        Teacher teacher = new Teacher(1L, "DOE", "John", createdAt, updatedAt);
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("martin.petit@test.com", "PETIT", "Martin", "password123", false)); 
+        users.add(new User("leon.bernard@test.com", "BERNARD", "Léon", "password123", false)); 
+
+        Session session = new Session(1L, "Lorem ipsum", sessionDate, "Suspendisse potenti. Praesent orci ligula, rhoncus ut semper ut, ullamcorper eget neque.", teacher, users, createdAt, updatedAt);
+
+        // Act
+        doNothing().when(sessionRepository).deleteById(session.getId());
+        sessionService.delete(session.getId());
+
+        // Assert
+        verify(sessionRepository).deleteById(session.getId());
     }
 }
