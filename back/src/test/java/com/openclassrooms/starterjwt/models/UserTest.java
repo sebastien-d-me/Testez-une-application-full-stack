74c0efae7d6254cde66876dd1440571c000d50a6
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-06 17:17:57 +0100

    ✅ Test Back
    - Ajout des tests sur le model uSER

--- back/src/test/java/com/openclassrooms/starterjwt/models/UserTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/models/UserTest.java
@@ -1,5 +1,113 @@
 package com.openclassrooms.starterjwt.models;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.UserService;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+
 public class UserTest {
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        createdAt = LocalDateTime.parse("2025-03-03T23:50:00");
+        updatedAt = LocalDateTime.parse("2025-03-04T00:04:30");
+    }
+
+
+    @Test
+    /// Test - Setter of the user
+    public void setter() {
+        // Arrange
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@test.com");
+        user.setLastName("DOE");
+        user.setFirstName("John");
+        user.setPassword("password");
+        user.setAdmin(false);
+        user.setCreatedAt(createdAt);
+        user.setUpdatedAt(updatedAt);
+
+        // Act
+        when(userRepository.save(user)).thenReturn(user);
+
+        // Assert
+        assertEquals(1L, user.getId());
+        assertEquals("john.doe@test.com", user.getEmail());
+        assertEquals("DOE", user.getLastName());
+        assertEquals("John", user.getFirstName());
+        assertEquals("password", user.getPassword());
+        assertEquals(false, user.isAdmin());
+        assertEquals("password", user.getPassword());
+        assertEquals(createdAt, user.getCreatedAt());
+        assertEquals(updatedAt, user.getUpdatedAt());
+    }
+
+
+    @Test
+    /// Test - Getter of the user
+    public void getter() {
+        // Arrange
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john.doe@test.com");
+        user.setLastName("DOE");
+        user.setFirstName("John");
+        user.setPassword("password");
+        user.setAdmin(false);
+        user.setCreatedAt(createdAt);
+        user.setUpdatedAt(updatedAt);
+
+        // Act
+        when(userRepository.save(user)).thenReturn(user);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        User userFound = userService.findById(1L);
+
+        // Assert
+        assertEquals(user.getId(), userFound.getId());
+        assertEquals(user.getEmail(), userFound.getEmail());
+        assertEquals(user.getLastName(), userFound.getLastName());
+        assertEquals(user.getFirstName(), userFound.getFirstName());
+        assertEquals(user.getPassword(), userFound.getPassword());
+        assertEquals(user.isAdmin(), userFound.isAdmin());
+        assertEquals(user.getCreatedAt(), userFound.getCreatedAt());
+        assertEquals(user.getUpdatedAt(), userFound.getUpdatedAt());
+    }
     
+
+    @Test
+    /// Test - Builder of the user
+    public void builderUser() {
+        // Arrange
+        User user = User.builder().id(1L).email("john.doe@test.com").lastName("DOE").firstName("John").password("password").admin(false).createdAt(createdAt).updatedAt(updatedAt).build();
+
+        // Act
+        when(userRepository.save(user)).thenReturn(user);
+
+        // Assert
+        assertEquals(1L, user.getId());
+        assertEquals("john.doe@test.com", user.getEmail());
+        assertEquals("DOE", user.getLastName());
+        assertEquals("John", user.getFirstName());
+        assertEquals("password", user.getPassword());
+        assertEquals(false, user.isAdmin());
+        assertEquals("password", user.getPassword());
+        assertEquals(createdAt, user.getCreatedAt());
+        assertEquals(updatedAt, user.getUpdatedAt());
+    }
 }
--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.*;
 import org.mockito.*;
@@ -13,6 +15,8 @@ import org.mockito.*;
 public class UserServiceTest {
     @InjectMocks
     private UserService userService;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 
     @Mock
@@ -22,6 +26,8 @@ public class UserServiceTest {
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
+        createdAt = LocalDateTime.parse("2025-03-03T23:50:00");
+        updatedAt = LocalDateTime.parse("2025-03-04T00:04:30");
     }
 
     
@@ -29,7 +35,7 @@ public class UserServiceTest {
     /// Test - Delete user by ID
     public void testDelete() {
         // Arrange
-        User user = new User("john.doe@test.com", "DOE", "John", "password", false); 
+        User user = new User(1L, "john.doe@test.com", "DOE", "John", "password", false, createdAt, updatedAt); 
 
         // Act
         doNothing().when(userRepository).deleteById(user.getId());
@@ -44,7 +50,7 @@ public class UserServiceTest {
     /// Test - Find user by ID
     public void testFindById() {
         // Arrange
-        User user = new User("john.doe@test.com", "DOE", "John", "password", false); 
+        User user = new User(1L, "john.doe@test.com", "DOE", "John", "password", false, createdAt, updatedAt); 
 
         // Act
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
