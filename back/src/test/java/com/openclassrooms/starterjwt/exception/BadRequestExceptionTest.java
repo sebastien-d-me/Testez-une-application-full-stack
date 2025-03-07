8ad950df4abed15ff15ced72b17bd77c3eee9ddb
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-07 01:29:02 +0100

    ✅ Test Back
    - Ajout des tests pour les exception

--- back/src/test/java/com/openclassrooms/starterjwt/exception/BadRequestExceptionTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/exception/BadRequestExceptionTest.java
@@ -1,5 +1,20 @@
 package com.openclassrooms.starterjwt.exception;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
 public class BadRequestExceptionTest {
-    
+    @Test
+    /// Test - Instance
+    public void testInstance() {
+        // Arrange
+        BadRequestException badRequestException = new BadRequestException();
+
+        // Act
+
+        // Assert
+        assertTrue(badRequestException instanceof RuntimeException);
+    }
 }
--- back/src/test/java/com/openclassrooms/starterjwt/exception/NotFoundExceptionTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/exception/NotFoundExceptionTest.java
@@ -1,5 +1,20 @@
 package com.openclassrooms.starterjwt.exception;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
 public class NotFoundExceptionTest {
-    
+    @Test
+    /// Test - Instance
+    public void testInstance() {
+        // Arrange
+        NotFoundException notFoundException = new NotFoundException();
+
+        // Act
+
+        // Assert
+        assertTrue(notFoundException instanceof RuntimeException);
+    }
 }
