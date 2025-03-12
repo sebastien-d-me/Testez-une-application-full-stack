188af9f7b95d8b15c6bdcb08565c3b62aaa6342a
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-03-12 23:17:26 +0100

    ✅ Test Back
    - Ajout de tests sur MessageResponse et JwtResponse

--- back/src/test/java/com/openclassrooms/starterjwt/payload/response/JwtResponseTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/payload/response/JwtResponseTest.java
@@ -1,5 +1,39 @@
 package com.openclassrooms.starterjwt.payload.response;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+
 public class JwtResponseTest {
-    
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    /// Test - Create a Token
+    public void testCreateToken() {
+        // Arrange
+        JwtResponse jwtResponse = new JwtResponse("", 1L, "", "", "", false);
+        jwtResponse.setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzM5NTQ2MTE3fQ.fNmj7-TytXWP1LrFBOCbNt0tIUUcK6KFcGXTWmtggCs");
+        jwtResponse.setType("Bearer");
+        jwtResponse.setId(2L);
+        jwtResponse.setUsername("john.doe@test.com");
+        jwtResponse.setFirstName("John");
+        jwtResponse.setLastName("DOE");
+        jwtResponse.setAdmin(true);
+
+        // Assert
+        assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzM5NTQ2MTE3fQ.fNmj7-TytXWP1LrFBOCbNt0tIUUcK6KFcGXTWmtggCs", jwtResponse.getToken());
+        assertEquals("Bearer", jwtResponse.getType());
+        assertEquals(2L, jwtResponse.getId());
+        assertEquals("john.doe@test.com", jwtResponse.getUsername());
+        assertEquals("John", jwtResponse.getFirstName());
+        assertEquals("DOE", jwtResponse.getLastName());
+        assertEquals(true, jwtResponse.getAdmin());
+    } 
 }
--- back/src/test/java/com/openclassrooms/starterjwt/payload/response/MessageResponseTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/payload/response/MessageResponseTest.java
@@ -1,5 +1,27 @@
 package com.openclassrooms.starterjwt.payload.response;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+
 public class MessageResponseTest {
-    
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    /// Test - Create a session
+    public void testSetMessage() {
+        // Arrange
+        MessageResponse messageResponse = new MessageResponse("");
+        messageResponse.setMessage("Test");
+
+        // Assert
+        assertEquals("Test", messageResponse.getMessage());
+    }
 }
